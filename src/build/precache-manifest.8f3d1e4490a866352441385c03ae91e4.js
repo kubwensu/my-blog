72de015a1dc892601643fcc62eb40bf5cf79dda7
Kubwensu <kubwensumambwe@yahoo.com>
2020-05-28 20:42:32 +0200

    React full stack. Mongo, Express, AWS

---
+++ src/build/precache-manifest.8f3d1e4490a866352441385c03ae91e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf8edde8eba28983084c17659caaa0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad15f689fae096f313e6",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "e7ed56f5b62204c80ca7",
+    "url": "/static/js/2.b1fb3659.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b1fb3659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad15f689fae096f313e6",
+    "url": "/static/js/main.b010b90b.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
